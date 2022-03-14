da34b298340ef45f86c6edce9d8eedbcba40ae64
maza_eksistence <ievakrumina93@gmail.com>
2022-03-14 21:53:26 +0100

    Update taskRepository unit test

--- app/src/test/java/com/ik/mvvmtaskapp/data/TaskRepositoryTest.kt
+++ app/src/test/java/com/ik/mvvmtaskapp/data/TaskRepositoryTest.kt
@@ -2,11 +2,12 @@ package com.ik.mvvmtaskapp.data
 
 import com.ik.mvvmtaskapp.ui.tasks.SortOrder
 import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import io.mockk.unmockkAll
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -24,11 +25,13 @@ class TaskRepositoryTest {
   }
 
   @Test
-  fun `get tasks`(){
-    //val flow = MutableStateFlow(listOf(Task(name = "First task")))
-    //every { taskDao.getTasks(any(), any(), any()) } returns flow
-    //val result = repo.getTasks("",SortOrder.BY_NAME, false)
-    //assertEquals("First task", result.collect { it.first().name })
+  fun `get tasks`() = runBlockingTest {
+    val taskFlow = flow {
+      emit(listOf(Task("First task")))
+    }
+    coEvery { taskDao.getTasks(any(), any(), any()) } returns taskFlow
+    val result = repo.getTasks("", SortOrder.BY_NAME, false).first()
+    assertEquals("First task", result.first().name)
   }
 
   @After
